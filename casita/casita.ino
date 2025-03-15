e0aabbe1fff22552793397a11079440f3b19262a
joseasanvil <46358777+joseasanvil@users.noreply.github.com>
2025-03-15 15:09:46 -0400

    Adding candle state

--- casita/casita.ino
+++ casita/casita.ino
@@ -4,6 +4,7 @@
 const int onOffSwitchPin = 2;   // Switch input pin
 const int stateSwitchPin = 3;   // Switch input pin
 const int bulbPin = 9;          // Output pin for the bulb (or relay)
+int bulbPower = 0;
 
 int onOffState = 1;             // Variable to store switch state
 int bulbState = 0;              // Bulb state
@@ -12,114 +13,16 @@ int numStates = 4;              // Number of states
 int stateSwitch = 0;
 int onOffSwitch = 0;
 
+int state4Counter = 0;
+
 void setup() {
     Serial.print("Setting up\n");
     pinMode(stateSwitchPin, INPUT_PULLUP);  // Enable internal pull-up resistor
     pinMode(onOffSwitchPin, INPUT_PULLUP);  // Enable internal pull-up resistor
     pinMode(bulbPin, OUTPUT);               // Set bulb pin as output
-    bulbState = EEPROM.read(EEPROM_ADDR);
-    setBulbState(bulbState);           // Ensure bulb starts OFF
+    bulbState = EEPROM.read(EEPROM_ADDR);   // Recover latest state from memory
+    setBulbState(bulbState);                // Ensure bulb starts on last recorded state
     Serial.begin(9600);
-}
-
-void setIntensityLED(int intensity){
-  analogWrite(bulbPin, intensity*onOffState);
-}
-
-void increaseLED() {
-  for(int i = 0; i <= 255; i++){
-    setIntensityLED(i);
-    delay(10);
-  }
-}
-
-void decreaseLED(){
-  for(int i = 255; i >= 0; i--){
-    setIntensityLED(i);
-    delay(10);
-  }
-}
-
-void switchOnOffState(int onOffSwitch){
-  if (onOffSwitch == LOW) {
-    if (onOffState == 0){
-      // Serial.print("Turning ON\n");
-      onOffState = 1;
-    }else{
-      // Serial.print("Turning OFF\n");
-      onOffState = 0;
-    }
-    if (bulbState == 0){
-      analogWrite(bulbPin, 255*onOffState);
-    }
-  }
-  delay(1000);
-}
-
-void state1(){
-  lightOn();
-}
-
-void state2(){
-  increaseLED();
-  decreaseLED();
-}
-
-void state3(){
-  lightOn();
-  delay(500);
-  lightOff();
-  delay(500);
-}
-
-void state4(){
-  int intensity = random(0, 255);
-  int waitTime = random(10, 1000);
-  setIntensityLED(intensity);
-  delay(waitTime);
-}
-
-void setBulbState(int bulbState){
-  if (bulbState == 0){
-    state1();
-  }
-  if (bulbState == 1){
-    state2();
-  }
-  if (bulbState == 2){
-    state3();
-  }
-  if (bulbState == 3){
-    state4();
-  }
-}
-
-void lightOn(){
-  analogWrite(bulbPin, 255*onOffState);
-}
-
-void lightOff(){
-  analogWrite(bulbPin, 0*onOffState);
-}
-
-void switchBulbState(int stateSwitch){
-  if (stateSwitch == LOW) {
-    // Serial.print("Switch triggered\n");
-    bulbState = bulbState + 1;
-    if (bulbState >= numStates){
-      bulbState = 0;
-    }
-    // Serial.print("Bulb state:");
-    // Serial.print(bulbState);
-    // Serial.print("\n");
-    if (EEPROM.read(EEPROM_ADDR) != bulbState) {
-        EEPROM.write(EEPROM_ADDR, bulbState);
-    }
-    if (bulbState == 0){
-      state1();
-    }
-    delay(1000);
-  }
 }
 
 void loop() {
@@ -132,12 +35,120 @@ void loop() {
       switchOnOffState(onOffSwitch);
     }
     if (bulbState == 1){
-      state2();
+      state1();
     }
     if (bulbState == 2){
-      state3();
+      state2();
     }
     if (bulbState == 3){
+      state3();
+    }
+    if (bulbState == 4){
       state4();
     }
+}
+
+void setBulbState(int bulbState){
+  if (bulbState == 0){
+    state0();
+  }
+  if (bulbState == 1){
+    state1();
+  }
+  if (bulbState == 2){
+    state2();
+  }
+  if (bulbState == 3){
+    state3();
+  }
+  if (bulbState == 4){
+    state4();
+  }
+}
+
+void switchBulbState(int stateSwitch){
+  if (stateSwitch == LOW) {
+    bulbState = bulbState + 1;
+    if (bulbState > numStates){
+      bulbState = 0;
+    }
+    if (EEPROM.read(EEPROM_ADDR) != bulbState) {
+        EEPROM.write(EEPROM_ADDR, bulbState);
+    }
+    if (bulbState == 0){
+      state0();
+    }
+    delay(1000);
+  }
+}
+
+void switchOnOffState(int onOffSwitch){
+  if (onOffSwitch == LOW) {
+    if (onOffState == 0){
+      onOffState = 1;
+    }else{
+      onOffState = 0;
+    }
+    if (bulbState == 0){
+      analogWrite(bulbPin, 255*onOffState);
+    }
+  }
+  delay(1000);
+}
+
+void state0(){ // State 0: Keep LED on
+  setIntensityLED(255);
+}
+
+void state1(){ // State 1: Dim LED on and off
+  dimLED(0,255);
+  dimLED(254,1);
+}
+
+void state2(){ // State 2: Suppose to work on/off LED with 1/2 a second intervals
+  setIntensityLED(255);
+  delay(500);
+  setIntensityLED(0);
+  delay(500);
+}
+
+void state3(){ // State 3: Simulate the LED to light like in a horror movie 
+  state4Counter = state4Counter + 1;
+  if (state4Counter <= 500){
+    int intensity = random(0, 255);
+    int waitTime = random(10, 600);
+    setIntensityLED(intensity);
+    delay(waitTime);
+  }else{
+    setIntensityLED(0); 
+    delay(5000);
+    dimLED(0,255);
+    state4Counter = 0;
+  }
+}
+
+void state4(){ // State 4: Simulate the LED to light like a candle
+  int intensity = random(0, 255);
+  int waitTime = random(10, 500);
+  dimLED(bulbPower,intensity);
+  delay(waitTime);
+}
+
+void setIntensityLED(int intensity){
+  bulbPower = intensity;
+  analogWrite(bulbPin, intensity*onOffState);
+}
+
+void dimLED(int start, int end){
+  if (start > end){
+    for(int i = start; i >= end; i--){
+      setIntensityLED(i);
+      delay(10);
+    }
+  }else{
+    for(int i = start; i <= end; i++){
+      setIntensityLED(i);
+      delay(10);
+    }
+  }
 }
