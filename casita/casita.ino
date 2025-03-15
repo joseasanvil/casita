f15b9e2c3ad2413b84a6d3d3d3d41f4423e5e87f
joseasanvil <46358777+joseasanvil@users.noreply.github.com>
2025-03-15 15:59:27 -0400

    updating candle state

--- casita/casita.ino
+++ casita/casita.ino
@@ -102,23 +102,23 @@ void switchOnOffState(){
   delay(1000);
 }
 
-void stateON(){ // State 0: Keep LED on
+void stateON(){ // Keep LED on
   setIntensityLED(255);
 }
 
-void stateDIM(){ // State 1: Dim LED on and off
+void stateDIM(){ // Dim LED on and off
   dimLED(0,255);
   dimLED(254,1);
 }
 
-void stateBLINK(){ // State 2: Suppose to work on/off LED with 1/2 a second intervals
+void stateBLINK(){ // Blink LED with 1/2 a second intervals
   setIntensityLED(255);
   delay(500);
   setIntensityLED(0);
   delay(500);
 }
 
-void stateHORROR(){ // State 3: Simulate the LED to light like in a horror movie 
+void stateHORROR(){ // Simulate the LED to light like in a horror movie 
   horrorCounter = horrorCounter + 1;
   int waitCounter = random(100, 1000);
   if (horrorCounter <= waitCounter){
@@ -135,11 +135,9 @@ void stateHORROR(){ // State 3: Simulate the LED to light like in a horror movie
   }
 }
 
-void stateCANDLE(){ // State 4: Simulate the LED to light like a candle
-  int intensity = random(0, 255);
-  int waitTime = random(10, 500);
+void stateCANDLE(){ // Simulate the LED to light like a candle
+  int intensity = random(10, 255);
   dimLED(bulbPower,intensity);
-  delay(waitTime);
 }
 
 void setIntensityLED(int intensity){
